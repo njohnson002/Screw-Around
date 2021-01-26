4440d6e7732f85b118c7b49d646d76b05207e4d5
mjc19b <mjc19b@my.fsu.edu>
2021-01-26 18:53:22 -0500

    not so empty cpp file, ALSO a bunch of new studd

--- funkystuff.cpp
+++ funkystuff.cpp
@@ -1,4 +1,17 @@
-//
-// Created by Mauricio Cabrera on 1/26/21.
-//
+#include <iostream>
+#include <string>
+#include "car.hpp"
 
+using namespace std;
+
+int main()
+{
+int x = 1;
+int y = 2;
+int z;
+cout << x << ", " << y << endl;
+z = x +y;
+cout << "added together, these are ";
+cout << z << endl;
+    return 0;
+}
